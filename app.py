6eb2fbd61fd6589119cb6e2a6c7e09c9499a920b
Kou-kun42 <shinigamithekido@gmail.com>
2021-02-03 14:20:44 -0500

    Added about, login, and signup routes and templates

--- app.py
+++ app.py
@@ -5,17 +5,36 @@ from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
 
-#Setup
+# Setup
 
 app = Flask(__name__)
 
 
-#Flask routes
+# Flask routes
 
 @app.route('/')
 def home_page():
     '''Display homepage'''
     return render_template('home.html')
+
+
+@app.route('/about')
+def home_page():
+    '''Display about page'''
+    return render_template('about.html')
+
+
+@app.route('/login')
+def home_page():
+    '''Display login page'''
+    return render_template('login.html')
+
+
+@app.route('/signup')
+def home_page():
+    '''Display signup page'''
+    return render_template('signup.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
