1992b8bb0cbcadd6d66e2fa88734ad4eb5317f46
Kou-kun42 <shinigamithekido@gmail.com>
2021-02-21 14:19:02 -0500

    Merging changes from db branch and reworking project structure

---
+++ proxcity_app/routes.py
@@ -0,0 +1,243 @@
+from flask import (
+    request,
+    redirect,
+    render_template,
+    url_for,
+    Blueprint,
+    session,
+    flash
+)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from pprint import PrettyPrinter
+from operator import itemgetter
+import datetime
+import requests
+import json
+import os
+from proxcity_app import bcrypt, mongo, app
+
+##########################################
+#                  Setup                 #
+##########################################
+
+load_dotenv()
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+
+pp = PrettyPrinter(indent=4)
+
+main = Blueprint("main", __name__)
+auth = Blueprint("auth", __name__)
+
+
+##########################################
+#           Session Config               #
+##########################################
+
+@app.before_request
+def before_request():
+    session.permanent = True
+    app.permanent_session_lifetime = datetime.timedelta(seconds=10)
+    session.modified = True
+
+
+##########################################
+#              Main Routes               #
+##########################################
+
+@main.route('/')
+def home():
+    '''Display homepage'''
+    return render_template('home.html')
+
+
+@main.route('/about')
+def about():
+    '''Display about page'''
+    return render_template('about.html')
+
+
+@main.route('/login')
+def login():
+    '''Display login page'''
+    return render_template('login.html')
+
+
+@main.route('/signup')
+def signup():
+    '''Display signup page'''
+    return render_template('signup.html')
+
+
+@main.route('/user')
+def user():
+    '''Display User Page'''
+    return render_template('user.html')
+
+
+@main.route('/results')
+def results():
+    '''Display result page'''
+    query = request.args.get("search-query")
+    city = request.args.get("city")
+    zipcode = request.args.get("zipcode")
+
+    # Looking up City and State using Ziptastic api
+    zipurl = "http://ZiptasticAPI.com/" + str(zipcode)
+    zip_json = requests.get(zipurl).json()
+    # Getting city and state from result json
+    city = zip_json["city"]
+    state = zip_json["state"]
+    location = f"{city}, {state}"
+
+    # Foursquare places api url
+    url = "https://api.foursquare.com/v2/venues/search"
+    # Parameters for the venues api
+    params = {
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+        "query": query,
+        "near": location,
+        "v": 20210201,
+        "limit": 1
+    }
+
+    results_json = requests.get(url, params=params).json()
+    results = results_json['response']['venues']
+
+    # Gets venue ids for additional info
+    venue_ids = []
+    for venue in results_json['response']['venues']:
+        venue_ids.append(venue['id'])
+
+    desc_params = {
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+        "v": 20210201
+    }
+
+    # Gets picture and description of each location
+    descriptions = []
+    photos = []
+    photo_prefix = 'https://fastly.4sqi.net/img/general/300x300'
+    for venue_id in venue_ids:
+        description_url = f"https://api.foursquare.com/v2/venues/{venue_id}"
+        description_results = requests.get(
+            description_url, params=desc_params).json()
+        pp.pprint(description_results)
+        # Tries to get picture and description
+        try:
+            description = description_results['response']['venue'][
+                'description']
+        # Uses a placeholder if info does not exist
+        except KeyError:
+            description = "No Description Given"
+        descriptions.append(description)
+        try:
+            photo = photo_prefix + description_results[
+                'response']['venue']['bestPhoto']['suffix']
+        except KeyError:
+            photo = url_for('static', filename='Logo.png')
+        photos.append(photo)
+
+    context = {
+        'results': results,
+        'num_venues': len(results),
+        'photos': photos,
+        'desc': descriptions
+    }
+    return render_template('results.html', **context)
+
+
+##########################################
+#             Auth Routes                #
+##########################################
+
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    '''Display signup page'''
+    if 'email' in session:
+        return redirect(url_for('main.home'))
+
+    if request.method == 'GET':
+        return render_template('signup.html')
+
+    if request.method == 'POST':
+        email, name, password = itemgetter(
+            'email', 'name', 'password')(request.form)
+
+        # Form input sanitization
+        if email is None or type(email) is not str or \
+                name is None or type(name) is not str or \
+                password is None or type(password) is not str:
+            flash('ERROR: Form input is not in the correct format')
+            return redirect(url_for('auth.signup'))
+
+        # Check if the user already exists in the database, if so go to login
+        user = mongo.db.users.find_one({'email': email})
+
+        if user is not None:
+            flash('ERROR: User already exists in the database.')
+            return redirect(url_for('auth.login'))
+
+        # Insert the user into the database
+        hashed_password = bcrypt.generate_password_hash(
+            password).decode('utf-8')
+        session['email'] = email
+
+        new_user = {
+            'email': email,
+            'name': name,
+            'password': hashed_password
+        }
+
+        mongo.db.users.insert_one(new_user)
+
+        return redirect(url_for('main.home'))
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    '''Display login page'''
+    if 'email' in session:
+        return redirect(url_for('main.home'))
+
+    if request.method == 'GET':
+        return render_template('login.html')
+
+    if request.method == 'POST':
+        email, password = itemgetter('email', 'password')(request.form)
+
+        # Form input sanitization
+        if (
+            email is None or type(email) is not str or
+            password is None or type(password) is not str
+           ):
+            flash('ERROR: Form input is not in the correct format')
+            return redirect(url_for('auth.login'))
+
+        user = mongo.db.users.find_one({'email': email})
+        print(user)
+
+        # If the user is not found in the database
+        if user is None:
+            flash('ERROR: User not found, please signup')
+            return redirect(url_for('auth.signup'))
+
+        # If the passwords do not match
+        if bcrypt.check_password_hash(user['password'], password):
+            flash('ERROR: Password is incorrect')
+            return redirect(url_for('auth.login'))
+
+        session['email'] = email
+        flash('Successfully logged in!')
+        return redirect(url_for('main.home'))
+
+
+@auth.route('/logout', methods=['GET'])
+def logout():
+    session.pop('email', None)
+    return redirect(url_for('auth,login'))
